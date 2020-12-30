5f42709a58b708836b4879d8cc331c0f02e9c37e
David Griffiths <dogriffiths@gmail.com>
2020-12-30 19:25:00 +0000

    feat: Add Radio control

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import Form from './Form'
 import Field from './Field'
 import FieldGroup from './FieldGroup'
 import useField from './Field/useField'
+import Radio from './Radio'
 import Select from './Select'
 import Text from './Text'
 import TextArea from './TextArea'
 
-export {Form, Field, FieldGroup, Select, Text, TextArea, useField}
+export {Form, Field, FieldGroup, Radio, Select, Text, TextArea, useField}
